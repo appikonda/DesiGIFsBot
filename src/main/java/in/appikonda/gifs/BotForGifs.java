ce6482b13e8fe30749d8991d3904e5bf9fa1f4fc
srikanth podishetty <srikanthp7@gmail.com>
2018-02-08 16:00:19 -0800

    did cherry pick srikanth's commit

--- src/main/java/in/appikonda/gifs/BotForGifs.java
+++ src/main/java/in/appikonda/gifs/BotForGifs.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 import org.json.JSONException;
 import org.telegram.telegrambots.api.methods.AnswerInlineQuery;
 import org.telegram.telegrambots.api.methods.send.SendDocument;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.inlinequery.result.InlineQueryResult;
@@ -26,51 +27,113 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class BotForGifs extends TelegramLongPollingBot {
-
+	AnswerInlineQuery answer;
+	JsonParser jp;
+	private int size;
 	public String getBotUsername() {
 		return "DesiGIFs_bot";
 	}
 
 	public void onUpdateReceived(Update update) {
-		if (update != null) {
-			Message message = update.getMessage();
-			     String inlineQuery =  update.getInlineQuery().getQuery();
-			     String queryId = update.getInlineQuery().getId();
-			if (inlineQuery != null && !inlineQuery.isEmpty()) {
-				AnswerInlineQuery answer = new AnswerInlineQuery();
-				JsonParser jp = new JsonParser();		
+		if (update!=null && update.getInlineQuery()!=null)
+		{
+		Message message = update.getMessage();
+		     String inlineQuery =  update.getInlineQuery().getQuery();
+		     String queryId = update.getInlineQuery().getId();
+		if (message != null && message.hasText() ) {
+			if (message.getText().contains("/gif")) {
+				JsonParser jp = new JsonParser();
+				String searchStg = new String("Sherya");
 				try {
-					List<String> allGifs = jp.getItAll(inlineQuery);
-					if (!allGifs.isEmpty() ){
-						List<InlineQueryResult> results = new ArrayList<InlineQueryResult>();
-							for (int i = 0; i < allGifs.size(); i++) {
-								InlineQueryResultGif inlineGif = new InlineQueryResultGif();
-								String s=   shuffle("abcdefghijklmnopqr");
-								inlineGif.setGifHeight(600);
-								inlineGif.setGifWidth(600);
-								inlineGif.setId(s);// Need a random string 
-								inlineGif.setThumbUrl(allGifs.get(i));
-								inlineGif.setGifUrl(allGifs.get(i));
-								results.add(inlineGif);
-							}
-							answer.setInlineQueryId(queryId);
-							answer.setResults(results);
-							answer.isPersonal();
-							execute(answer);
+					List<String> allGifs = jp.getItAll(searchStg);
+
+					// Create ReplyKeyboardMarkup object
+					ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+					// Create the keyboard (list of keyboard rows)\
+					// Create a keyboard row
+					KeyboardRow row = new KeyboardRow();
+					List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+					for (int i = 0; i < allGifs.size(); i++) {
+						row.add(allGifs.get(i));
+						keyboard.add(row);
 					}
-				} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+
+					
+					// Set the keyboard to the markup
+					keyboardMarkup.setKeyboard(keyboard);
+					// Add it to the message
+
+					// InlineKeyboardButton
+					InlineKeyboardButton inline = new InlineKeyboardButton();
+					for (int i = 0; i < allGifs.size(); i++) {
+						inline.setUrl(allGifs.get(i));
+					}
+					SendDocument msg = new SendDocument().setChatId(message.getChatId()).setDocument(allGifs.get(1));
+
+					// sendMessageRequest.setChatId();
+					try {
+						sendDocument(msg); // Sending our message object to user
 					} catch (TelegramApiException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
 
+			}
+		}
 		
+		else if (inlineQuery != null && !inlineQuery.isEmpty()) {
+			answer = new AnswerInlineQuery();
+			jp = new JsonParser();		
+			try {
+				List<String> allGifs = jp.getItAll(inlineQuery);
+				System.out.println("GIFFSSSSIZE:" + allGifs.size());
+				if (allGifs.size()>10)
+				{
+					size = 9 ;
+				}
+				else
+				{
+					size = allGifs.size();
+				}
+					
+				if (!allGifs.isEmpty() ){
+					List<InlineQueryResult> results = new ArrayList<InlineQueryResult>();
+						for (int i = 0; i < size; i++) {
+							InlineQueryResultGif inlineGif = new InlineQueryResultGif();
+							String s=   shuffle("s4324abcdefghgghfijklmnopqr");
+							inlineGif.setGifHeight(600);
+							inlineGif.setGifWidth(600);
+							inlineGif.setId(s);// Need a random string 
+							inlineGif.setThumbUrl(allGifs.get(i));
+							inlineGif.setGifUrl(allGifs.get(i));
+							results.add(inlineGif);
+						}
+						answer.setInlineQueryId(queryId);
+						answer.setCacheTime(10000);
+						System.out.println("QUERYIDDDDDDDDDDDDDDDDDDDDDDDDDDD" + queryId);
+						answer.setResults(results);
+						answer.isPersonal();
+						this.execute(answer);
+						answer = null;
+				}
+			} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (TelegramApiException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
